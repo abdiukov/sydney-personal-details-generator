8d1f15e959f9b78743dd584035e49f7b193857cb
A-Abdiukov <a.abdiukov@gmail.com>
2021-05-23 03:10:57 +1000

    update UI

--- SydneyIdentityGenerator/UI/MainWindow.xaml.cs
+++ SydneyIdentityGenerator/UI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 
 namespace UI
 {
@@ -7,9 +10,23 @@ namespace UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private protected Regex regex = new("[^0-9]+");
+
         public MainWindow()
         {
             InitializeComponent();
+            Cb_AvailableRegions.ItemsSource = new List<string> { "Sydney, NSW" };
+            Cb_AvailableRegions.SelectedIndex = 0;
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Btn_GenerateCSV_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
